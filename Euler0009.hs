25c6eba7f486db561f0227b43ae620289a3ad8ad
Stuart Olsen <so1132@txstate.edu>
2015-07-23 02:26:52 -0500

    Added 9

--- Euler0009.hs
+++ Euler0009.hs
@@ -1,3 +1,12 @@
 module Euler0009 where
 
-solution = 0
+triples :: Integral a => [(a,a,a)]
+triples = do
+  a <- [0..1000]
+  b <- [a..1000-a]
+  let c = 1000 - a - b
+  if c > b && a^2 + b^2 == c^2
+    then return (a,b,c)
+    else []
+
+solution = let (a,b,c) = triples !! 0 in a*b*c
