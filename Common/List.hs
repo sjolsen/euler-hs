ff99eaec836da9401839874d6d26e78408256d2e
Stuart Olsen <so1132@txstate.edu>
2015-07-23 02:18:16 -0500

    Added 8

--- Common/List.hs
+++ Common/List.hs
@@ -13,3 +13,19 @@ upTo l n = l `whileSatisfying` (<= n)
 
 below :: Ord a => [a] -> a -> [a]
 below l n = l `whileSatisfying` (< n)
+
+-- Subsequences
+
+takeMaybe :: Int -> [a] -> Maybe [a]
+takeMaybe n l =
+  let j = take n l in
+  if length j == n
+    then Just j
+    else Nothing
+
+adjacentSubsequences :: Int -> [a] -> [[a]]
+adjacentSubsequences 0 _      = [[]]
+adjacentSubsequences _ []     = []
+adjacentSubsequences n (x:xs) = case takeMaybe n (x:xs) of
+  Just l  -> l : adjacentSubsequences n xs
+  Nothing -> []
