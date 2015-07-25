9a6468aedcaf0b8b2348de3a74c8662539e5e95b
Stuart Olsen <so1132@txstate.edu>
2015-07-24 21:52:07 -0500

    Better, but still slow, solution to 23

--- Euler0023.hs
+++ Euler0023.hs
@@ -7,12 +7,15 @@ import Data.List
 abundant :: Integral a => a -> Bool
 abundant n = sum (pdivisors n) > n
 
+limit :: Integral a => a
+limit = 28123
+
 abundantNumbers :: Integral a => [a]
 abundantNumbers = filter abundant [1..]
 
 abundantSums :: Integral a => [a]
-abundantSums = uniq $ sort [a+b | a <- abundantNumbers `upTo` 28123
-                                , b <- abundantNumbers `upTo` (28123 - a)]
+abundantSums = uniq $ sort [a+b | a <- abundantNumbers `upTo` limit
+                                , b <- abundantNumbers `from` a `upTo` (limit - a)]
 
 solution :: IO Integer
 solution = return . sum $ [1..28123] `setMinus` abundantSums
